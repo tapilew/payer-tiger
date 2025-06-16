d3e60ca1b8c4f4ebb3d97567dd1daac5c3b75ce9
Luis Tapia <86934099+tapilew@users.noreply.github.com>
2025-06-16 00:15:58 -0500

    fix: pls build this time :(

--- app/next.config.js
+++ app/next.config.js
@@ -10,48 +10,13 @@ const nextConfig = {
     };
 
     if (!isServer) {
-      // Add rule to handle HeartbeatWorker as raw asset to avoid Terser processing
-      config.module.rules.push({
-        test: /HeartbeatWorker\.js$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/js/[name].[contenthash][ext]',
-        },
-      });
-
-      // Handle Sherry SDK HeartbeatWorker minification issue
-      if (config.optimization.minimizer) {
-        config.optimization.minimizer = config.optimization.minimizer.map((minimizer) => {
-          if (minimizer.constructor.name === 'TerserPlugin') {
-            return new minimizer.constructor({
-              ...minimizer.options,
-              test: /\.js(\?.*)?$/i,
-              exclude: [
-                /HeartbeatWorker/i,
-                /\.worker\.js$/i,
-                /node_modules\/@sherrylinks\/sdk.*HeartbeatWorker.*\.js$/i,
-              ],
-              terserOptions: {
-                parse: {
-                  ecma: 2020,
-                },
-                compress: {
-                  ...minimizer.options.terserOptions?.compress,
-                  module: false,
-                  drop_console: false,
-                },
-                mangle: {
-                  safari10: true,
-                },
-                format: {
-                  comments: false,
-                },
-              },
-            });
-          }
-          return minimizer;
-        });
-      }
+      // Completely disable Terser minification to avoid HeartbeatWorker issues
+      config.optimization.minimize = false;
+      
+      // Alternative: If we want to keep minification for other files, replace Terser
+      // config.optimization.minimizer = config.optimization.minimizer.filter(
+      //   minimizer => minimizer.constructor.name !== 'TerserPlugin'
+      // );
     }
 
     return config;
