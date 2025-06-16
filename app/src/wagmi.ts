485c58d27ea9ccf4933d64353e751cd06dd4026b
Luis Tapia <86934099+tapilew@users.noreply.github.com>
2025-06-16 02:27:15 -0500

    fix: upgrade next.js 15 and remove wc

--- app/src/wagmi.ts
+++ app/src/wagmi.ts
@@ -1,17 +1,13 @@
 import { cookieStorage, createConfig, createStorage, http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
-import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
+import { coinbaseWallet, injected } from "wagmi/connectors";
 
 export function getConfig() {
-  const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
-  if (!projectId) throw new Error("NEXT_PUBLIC_WC_PROJECT_ID is not set");
-
   return createConfig({
     chains: [mainnet, sepolia],
     connectors: [
       injected(),
       coinbaseWallet(),
-      walletConnect({ projectId }),
     ],
     storage: createStorage({
       storage: cookieStorage,
